e8b527e13dce3aaf0a002bcbfc9593a59a1240c1
Renegade Asturias <renegade.asturias@gmail.com>
2022-02-07 22:23:15 +0100

    Proyecto directivas ngIf, ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivas';
+
+  titulo = 'Registro de Usuarios';
+  mensaje = '';
+  registrado = false;
+  nombre:string = "";
+  apellido:string = "";
+  entradas:object[];
+
+  registrarUsuario(){
+    this.registrado=true;
+    this.mensaje="Usuario registrado con éxito!";
+  }
+
+  constructor(){
+    this.entradas = [
+      {titulo:"Python cada día más presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"JavaScript cada vez más funcional"},
+      {titulo:"Kotlin pontencia para tus apps"},
+      {titulo:"¿Dónde quedó Pascal?"},
+    ]
+
+  }
+
+
+
 }
